3c819c4f4289dbd7776a4c0c9324e27658d9c528
James Korin <jpkorin@gmail.com>
2024-02-07 15:58:02 -0500

    fonts and icons

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-import { Button, FlatList, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native'
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native'
 import { LinearGradient } from "expo-linear-gradient"
 import Logo from './assets/github-mark.svg'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 import RepoCard from './components/RepoCard'
+import SearchBar from './components/SearchBar'
+import { useFonts } from 'expo-font'
 
 export default function App() {
 
@@ -14,6 +16,11 @@ export default function App() {
 
   const [searchQuery, setSearchQuery] = useState('tetris+language:assembly&sort=stars&order=desc');
   const [repos, setRepos] = useState([]);
+
+  const [fontsLoaded] = useFonts({
+    'SF-Pro-Display-Regular': require('./assets/fonts/SF-Pro-Display-Regular.otf'),
+    'SF-Pro-Display-Bold': require('./assets/fonts/SF-Pro-Display-Bold.otf'),
+  });
 
   const search =async ()=> {
     const url = `https://api.github.com/search/repositories?q=${searchQuery}&per_page=20`;
@@ -26,16 +33,14 @@ export default function App() {
         }
       });
       setRepos(r.data.items);
-      console.log(r.data.items.length);
     } catch (err) {
-      console.log(JSON.stringify(err));
+      console.log(JSON.stringify(err, null, 2));
     }
   }
 
   return (
+      
       <LinearGradient colors={['#e2dcee', '#f1f1f1']} style={styles.linearGradient}>
-
-        <SafeAreaView>
 
         {/* Header */}
         <View style={styles.header}>
@@ -44,13 +49,11 @@ export default function App() {
             height="100%" 
             viewBox={`0 0 ${originalWidth} ${originalHeight}`}/>
           </View>
-          <Text style={styles.title}>GitHub Repo Search</Text>
+          <Text style={{fontFamily: 'SF-Pro-Display-Bold', ...styles.title}}>GitHub Repo Search</Text>
         </View>
 
         {/* Search bar */}
-        <View style={styles.searchContainer}>
-          <TextInput placeholder='Search' style={styles.searchInput} onChangeText={text => setSearchQuery(text)} value={searchQuery}/>
-        </View>
+        <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
 
         <Button title={'Seach'}
         onPress={search}/>
@@ -66,24 +69,11 @@ export default function App() {
           onPress={() => console.log(item.name)}/>
         )}
         />
-
-        </SafeAreaView>
       </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  searchContainer: {
-    width: "100%",
-    marginTop: 20
-  },
-  searchInput: {
-    width: "100%",
-    backgroundColor: "#FAFAFA",
-    padding: 10,
-    borderRadius: 9,
-    color: "#000000"
-  },
   header: {
     marginTop: 75,
     flexDirection: "row",
@@ -101,17 +91,19 @@ const styles = StyleSheet.create({
   },
   linearGradient: {
     alignItems: "center",
-    backgroundColor: "red",
+    width: "100%",
+    height: "100%",
     paddingHorizontal: 30
   },
   cardList: {
     width: "100%",
-
+    paddingBottom: 60
   }  
 });
 
-
-// Make the call to search
+// Update font
+// Highlight search term in titles and descriptions in results page
+// Construct search query (search for terms in names, readmes, etc. instead of just submitting the keyword)
+// Collapse the header on scroll
 // Debounce on search input update
-// Display the list of results
 // Navigate to second page with details
---
+++ components/SearchBar.js
@@ -0,0 +1,44 @@
+import { StyleSheet, TextInput, View } from 'react-native'
+import SearchIcon from '../assets/search.svg'
+import ClearSearchIcon from '../assets/x.svg'
+import { useFonts } from 'expo-font'
+
+export default function SearchBar({
+    searchQuery,
+    setSearchQuery
+}) {
+
+    const [fontsLoaded] = useFonts({
+        'SF-Pro-Display-Regular': require('../assets/fonts/SF-Pro-Display-Regular.otf'),
+        'SF-Pro-Display-Bold': require('../assets/fonts/SF-Pro-Display-Bold.otf'),
+    });
+
+    return <View style={styles.searchContainer}>
+
+        <SearchIcon/>
+
+        <TextInput placeholder='Search' 
+        style={{fontFamily: 'SF-Pro-Display-Regular', ...styles.searchInput}} 
+        onChangeText={text => setSearchQuery(text)} 
+        value={searchQuery}/>
+
+        <ClearSearchIcon onPress={() => setSearchQuery('')}/>
+    </View>;
+}
+
+const styles = StyleSheet.create({
+    searchContainer: {
+        backgroundColor: "#edebf4",
+        paddingHorizontal: 14,
+        paddingVertical: 14,
+        borderRadius: 9,
+        color: "#000000",
+        marginTop: 20,
+        marginHorizontal: 30,
+        flexDirection: "row"
+    },
+    searchInput: {
+      width: "100%",
+      marginLeft: 14
+    } 
+  });
